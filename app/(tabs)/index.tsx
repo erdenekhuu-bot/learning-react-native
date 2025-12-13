fcedc42c64e6ba12ac9adfb14ce0545fc3643d59
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2025-12-13 17:12:37 +0800

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,39 @@
-import { Image } from 'expo-image';
-import { Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Image } from "expo-image";
+import { Text, View, TextInput, SafeAreaView } from "react-native";
+import { Link } from "expo-router";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [text, onChangeText] = useState("");
+
   return (
-    <View style={{ flex: 1,  justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={{ fontSize: 30, color: 'white' }}>HOME SCREEN</Text>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "white"
+      }}
+    >
+      <View>
+        <View style={{marginVertical: 50, paddingHorizontal: 20}}>
+          <TextInput
+            style={{
+              borderColor: "gray",
+              borderRadius: 10,
+              borderWidth: 1,
+              marginBottom: 10,
+              paddingHorizontal: 10,
+            }}
+            onChangeText={onChangeText}
+            value={text}
+          />
         </View>
+        <View>
+          <Text style={{ fontSize: 30, textAlign: "center" }}>HOME SCREEN</Text>
+        </View>
+        <View>
+          <Text>Lorem 132</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
