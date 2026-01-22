3ba4d9e4ba91d69ec4a5a75c8bbc22c99278b2b0
erdenekhuu.e <erdenekhuu.e@gmobile.mn>
2026-01-22 17:35:17 +0800

    .

--- app/(tabs)/cart.tsx
+++ app/(tabs)/cart.tsx
@@ -2,6 +2,8 @@ import { Text, View, TextInput, SafeAreaView } from "react-native";
 import { useState, useEffect } from "react";
 import { FlatList } from "react-native";
 import { VStack } from "@/components/ui/vstack";
+import { Card } from "@/components/ui/card";
+import { Image } from "@/components/ui/image";
 
 export default function CartScreen() {
   const [lists, setList] = useState([]);
@@ -37,17 +39,27 @@ export default function CartScreen() {
       }}
     >
       <VStack space="2xl">
-        <View className="block">
+        <View style={{ marginVertical: 30 }}>
           <Text className="text-2xl font-bold p-4">Your order</Text>
         </View>
         <FlatList
           data={lists}
           keyExtractor={(item) => item._id}
           renderItem={({ item }) => (
-            <View className="p-4 border-b border-gray-200 w-full">
-              <Text className="text-lg font-semibold">{item.name}</Text>
-              <Text className="text-gray-600">Price: ${item.price}</Text>
-            </View>
+            <Card key={item._id} variant="outline" className="m-4 p-4">
+              {/* <View className="flex flex-row justify-between items-center">
+                <Text className="font-bold text-lg">{item.name}</Text>
+                <Text className="font-bold">${item.price}</Text>
+              </View> */}
+              <Image
+                source={{
+                  uri:
+                    `${process.env.EXPO_PUBLIC_API_URL}` + item?.product?.image,
+                }}
+                className="block w-32 h-32 rounded-xl"
+                alt=""
+              />
+            </Card>
           )}
         />
       </VStack>
